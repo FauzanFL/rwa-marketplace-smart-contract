7de4d6d5b3f04ed48905d3b22529aeaeb5be8732
fauzan.lisan <fauzan.lisan@solusi247.com>
2025-05-23 09:15:55 +0700

    Add comment

--- scripts/send-eth.js
+++ scripts/send-eth.js
@@ -4,8 +4,8 @@ async function main() {
   const [sender] = await ethers.getSigners();
 
   const tx = await sender.sendTransaction({
-    to: "0xDbD8c255281cf1f386A2Fd5799488ccA56b90be8",
-    value: ethers.parseEther("10")
+    to: "0xDbD8c255281cf1f386A2Fd5799488ccA56b90be8", // Your address destination
+    value: ethers.parseEther("10") // 10 ETH
   });
 
   console.log("Transaction hash:", tx.hash);
